aea8269e0e0e55ffe01bc5b30ca52b04abda9ebe
Rene Avila <reneavila1993@gmail.com>
2020-05-25 19:36:14 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,15 +1,7 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
-<<<<<<< HEAD
     def change
         create_table :students do |t|
             t.string :name
         end
     end
-=======
-  def change
-    create_table :students do |t|
-      t.string :name 
-    end
-  end
->>>>>>> ce0b1218e2da2a76cb5f165849d52b1cc5552412
 end
